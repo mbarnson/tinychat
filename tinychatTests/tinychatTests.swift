a32f52000b0a711530978019beb04d43b8cc83c6
Matt Barnson <matthew@barnson.org>
2026-07-01 18:11:34 -0700

    Initial Commit

---
+++ tinychatTests/tinychatTests.swift
@@ -0,0 +1,18 @@
+//
+//  tinychatTests.swift
+//  tinychatTests
+//
+//  Created by Matthew Barnson on 7/1/26.
+//
+
+import Testing
+
+struct tinychatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
